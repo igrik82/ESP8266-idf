f49a9c88aa7eefd8ec3610398bb3b5901bb1ea0a
Игорь <igrik82@gmail.com>
2025-01-08 20:38:23 +0500

    Initial commit

---
+++ 06_wifi_mqtt/clean.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Clean old directories
+rm -rf .cache build
+echo "=================================================="
+echo "|       Output message may contain errors        |"
+echo "|       for project esp8266 execute after        |"
+echo "|                                                |"
+echo "|       source venv/bin/activate && make         |"
+echo "=================================================="
+echo ""
