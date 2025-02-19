d983447a1593bb9e45613bb1e28498bf42f3453e
Игорь <igrik82@gmail.com>
2025-02-19 11:36:20 +0500

    Small changes

--- 06_wifi_mqtt/clean.sh
+++ 06_wifi_mqtt/clean.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
 # Clean old directories
-rm -rf .cache build
-echo "=================================================="
-echo "|       Output message may contain errors        |"
-echo "|       for project esp8266 execute after        |"
-echo "|                                                |"
-echo "|       source venv/bin/activate && make         |"
-echo "=================================================="
-echo ""
+rm -rf .cache build venv
