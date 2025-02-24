3fc2b22e4c0d4835ca2743f5c82b43c12eb335df
Игорь <igrik82@gmail.com>
2025-02-24 18:31:46 +0500

    Working wifi mqtt ds18b20

---
+++ 07_wifi_mqtt_ds18b2/main/main.cpp
@@ -0,0 +1,160 @@
+#include "FreeRTOS.h" // IWYU pragma: keep
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_system.h"
+#include "gpio.h" // IWYU pragma: keep
+#include "mqtt.h"
+#include "projdefs.h"
+#include "wifi_simple.h"
+#include <cstddef>
+#include <cstdio>
+
+#define SENSOR_COUNT 2
+
+// ============================ Global variables ==============================
+
+// Structures for sensor
+typedef struct {
+    OneWire::DS18B20& onewire_pin;
+    // float& temp;
+    uint8_t (*sensor_addr)[8];
+} Temp_param;
+
+EventGroupHandle_t common_event_group = xEventGroupCreate();
+QueueHandle_t temperature_queue = xQueueCreate(5, sizeof(SensorData_t));
+
+uint8_t _wifi_connect_bit { BIT0 };
+uint8_t _wifi_disconnect_bit { BIT1 };
+uint8_t _wifi_got_ip { BIT2 };
+uint8_t _wifi_lost_ip { BIT3 };
+
+// ===================== Functions ===========================================
+TaskHandle_t wifi_connection_handle = NULL;
+void wifi_connection(void* pvParameter)
+{
+    Wifi_NS::Wifi* wifi_obj = (Wifi_NS::Wifi*)pvParameter;
+    for (;;) {
+        wifi_obj->start();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+TaskHandle_t mqtt_connection_handle = NULL;
+void mqtt_connection(void* pvParameter)
+{
+    for (;;) {
+        Mqtt_NS::Mqtt mqtt(temperature_queue);
+        mqtt.start();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+// Get temperature task
+TaskHandle_t get_temperature_handle = NULL;
+
+void get_temperature(void* pvParameter)
+{
+    Temp_param* temp_param = (Temp_param*)pvParameter;
+    SensorData_t sensor_data = {};
+    for (;;) {
+        vTaskDelay(pdMS_TO_TICKS(2000));
+        for (uint8_t i = 0; i < SENSOR_COUNT; i++) {
+            sensor_data.sensor_id = i;
+            temp_param->onewire_pin.get_temp(temp_param->sensor_addr[i],
+                sensor_data.temperature);
+
+            printf("Temperature %d: %.2f\n", i, sensor_data.temperature);
+
+            xQueueSend(temperature_queue, &sensor_data, portMAX_DELAY);
+        }
+    }
+}
+
+// ===================== Debugging ============================================
+void checkStackUsage(void* pvParameter)
+{
+    {
+        for (;;) {
+            UBaseType_t highWaterMark = uxTaskGetStackHighWaterMark(wifi_connection_handle);
+            printf("High Water Mark:%u\n", highWaterMark);
+            vTaskDelay(pdMS_TO_TICKS(5000));
+        }
+    }
+}
+void printHeapInfo()
+{
+    // Print heap info
+    uint32_t freeHeap = esp_get_free_heap_size();
+    uint32_t freeHeapMin = esp_get_minimum_free_heap_size();
+    printf("Free Heap Size: %u bytes\n", freeHeap);
+    printf("Minimal Heap Size: %u bytes\n", freeHeapMin);
+}
+
+void heapMonitor(void* pvParameter)
+{
+    while (1) {
+        printHeapInfo();
+        vTaskDelay(pdMS_TO_TICKS(10000));
+    }
+}
+
+// ============================================================================
+// ============================= Main cycle ===================================
+// ============================================================================
+
+extern "C" void app_main(void)
+{
+    //============================ Logging ====================================
+    // Set log level in this scope
+    // [[maybe_unused]] const char* TAG = "Wi-Fi";
+    // esp_log_level_set(TAG, ESP_LOG_INFO);
+    // esp_log_level_set("*", ESP_LOG_VERBOSE);
+    esp_log_level_set("*", ESP_LOG_ERROR);
+
+    //========================== Initialization ===============================
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    // Wifi initialization
+    Wifi_NS::Wifi wifi;
+
+    // DS18B20 initialization
+    OneWire::DS18B20 onewire_pin { GPIO_NUM_12 };
+    // Uncomment this to read ROM address of DS18B20, one sensor at a time
+    // onewire_pin.readROM();
+
+    // Array of DS18B20 addresses
+    uint8_t ds18b20_address[SENSOR_COUNT][8] = {
+        // Left temperature sensor
+        { 0x28, 0xf5, 0x48, 0x16, 0x00, 0x00, 0x00, 0x61 },
+        // Right temperature sensor
+        { 0x28, 0x1c, 0xc1, 0x11, 0x00, 0x00, 0x00, 0x60 },
+        // ...
+    };
+
+    Temp_param param = { onewire_pin, ds18b20_address };
+
+    // //======================== Tasks looping ==================================
+    xTaskCreate(wifi_connection, "Wifi", 1024 * 2, &wifi, 5,
+        &wifi_connection_handle);
+
+    xTaskCreate(mqtt_connection, "Mqtt", 1024 * 2, NULL, 5,
+        &mqtt_connection_handle);
+
+    xTaskCreate(get_temperature, "Temperature", 1024 * 2, &param, 5,
+        &get_temperature_handle);
+
+    // Debug tasks
+    // xTaskCreate(checkStackUsage, "CheckStack", 1024 * 2, NULL, 5, NULL);
+    // xTaskCreate(heapMonitor, "HeapMonitor", 1024 * 2, NULL, 5, NULL);
+
+    // ======================== FreeRTOS specific =============================
+    for (;;) {
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+
+    vTaskStartScheduler();
+}
---
+++ 07_wifi_mqtt_ds18b2/main/mqtt.cpp
@@ -0,0 +1,131 @@
+#include "mqtt.h"
+#include "esp_event.h"
+#include "mqtt_client.h"
+#include "portmacro.h"
+
+// External variables from main
+extern EventGroupHandle_t common_event_group;
+extern uint8_t _wifi_connect_bit;
+extern uint8_t _wifi_disconnect_bit;
+extern uint8_t _wifi_got_ip;
+extern uint8_t _wifi_lost_ip;
+
+namespace Mqtt_NS {
+
+// Static variables
+esp_mqtt_client_config_t Mqtt::mqtt_cfg {};
+Mqtt::state_m Mqtt::_state { Mqtt::state_m::NOT_INITIALISED };
+
+Mqtt::Mqtt(QueueHandle_t& temperature_queue)
+{
+    // Queue from sensor
+    _sensor_queue = &temperature_queue;
+
+    // Client initialisation
+    if (_state == state_m::NOT_INITIALISED) {
+        mqtt_cfg.uri = CONFIG_BROKER_URL;
+        mqtt_cfg.port = CONFIG_BROKER_PORT;
+        mqtt_cfg.client_id = CONFIG_CLIENT_ID;
+        mqtt_cfg.username = MQTT_USER;
+        mqtt_cfg.password = MQTT_PASSWORD;
+        mqtt_cfg.keepalive = CONFIG_MQTT_KEEP_ALIVE;
+        mqtt_cfg.protocol_ver = MQTT_PROTOCOL_V_3_1_1;
+        ESP_LOGI(TAG, "MQTT client init");
+        client = esp_mqtt_client_init(&mqtt_cfg);
+        esp_mqtt_client_register_event(client, MQTT_EVENT_ANY, mqtt_event_handler,
+            NULL);
+
+        _state = state_m::INITIALISED;
+    }
+}
+
+// Evens callback
+esp_err_t Mqtt::mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
+{
+    ESP_LOGI(TAG, "in event %d", event->event_id);
+    // esp_mqtt_client_handle_t client = event->client;
+    switch (event->event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "Connected to MQTT broker at %s:%d", mqtt_cfg.uri,
+            mqtt_cfg.port);
+        ESP_LOGI(TAG, "Login - \"%s\" password - \"%s\"", mqtt_cfg.username,
+            mqtt_cfg.password);
+        // msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1,
+        // 0);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "Disconnected from MQTT broker at %s:%d", mqtt_cfg.uri,
+            mqtt_cfg.port);
+        // Client cannot be stopped from task!
+        // esp_mqtt_client_stop(client);
+        break;
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        break;
+    case MQTT_EVENT_DATA:
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+    return ESP_OK;
+}
+
+// Events handler
+void Mqtt::mqtt_event_handler(void* handler_args, esp_event_base_t base,
+    int32_t event_id, void* event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base,
+        event_id);
+    mqtt_event_handler_cb((esp_mqtt_event_handle_t)event_data);
+}
+
+// Start MQTT client
+void Mqtt::start()
+{
+    // wait for WI-FI connection and IP
+    EventBits_t bits = xEventGroupWaitBits(common_event_group,
+        _wifi_got_ip | _wifi_disconnect_bit,
+        pdFALSE, pdFALSE, portMAX_DELAY);
+    if (bits & _wifi_got_ip) {
+        ESP_LOGI(TAG, "[APP] Startup..");
+        ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+        esp_mqtt_client_start(client);
+        ESP_LOGI(TAG, "Client started");
+        xEventGroupClearBits(common_event_group, _wifi_got_ip);
+
+        // Publish sensor data
+        SensorData_t sensor_data = {};
+        for (;;) {
+            if (xQueueReceive(*_sensor_queue, &sensor_data, portMAX_DELAY) == pdTRUE) {
+
+                if (sensor_data.temperature == 0.0f && sensor_data.sensor_id == 0) {
+                    ESP_LOGW(TAG, "Получено нулевое значение от датчика %d",
+                        sensor_data.sensor_id);
+                    continue; // Пропустить отправку
+                }
+                char msg[10]; // buffer for message
+                snprintf(msg, sizeof(msg), "%.2f", sensor_data.temperature);
+
+                char topic[50]; // buffer for topic
+                snprintf(topic, sizeof(topic), "/topic/%d", sensor_data.sensor_id);
+
+                ESP_LOGI(TAG, "Temperature from MQTT: %s %s", msg, topic);
+                esp_mqtt_client_publish(client, topic, msg, 0, 0, 0);
+            }
+            // vTaskDelay(pdMS_TO_TICKS(100));
+        }
+    }
+}
+
+} // namespace Mqtt_NS
