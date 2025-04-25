cd23ddc791094d3a923f4293cbf825ec06aa15b0
Игорь <igrik82@gmail.com>
2025-04-25 11:07:17 +0500

    Feat: Web page now in spiffs

--- 10_NVS_http/main/http.cpp
+++ 10_NVS_http/main/http.cpp
@@ -1,10 +1,41 @@
 #include "http.h"
-#include "http_web_page.h"
+#include "esp_http_server.h"
 
 namespace Http_NS {
 
+httpd_handle_t* HttpServer::_server = NULL;
+
 HttpServer::HttpServer(void)
 {
+    // initialize and mounting SPIFFS
+    esp_vfs_spiffs_conf_t spiffs_config;
+    spiffs_config.base_path = "/spiffs";
+    spiffs_config.partition_label = "storage";
+    spiffs_config.max_files = 5;
+    spiffs_config.format_if_mount_failed = true;
+    esp_err_t ret = esp_vfs_spiffs_register(&spiffs_config);
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG_SPIFF, "Failed to mount or format filesystem");
+        } else {
+            ESP_LOGE(TAG_SPIFF, "Failed to initialize SPIFFS (%s)",
+                esp_err_to_name(ret));
+        }
+        return;
+    }
+    ESP_LOGI("SPIFFS", "Finished mounting SPIFFS");
+
+    // get SPIFFS partition information
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info(spiffs_config.partition_label, &total, &used);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG_SPIFF, "Failed to get SPIFFS partition information (%s)",
+            esp_err_to_name(ret));
+    } else {
+        ESP_LOGI(TAG_SPIFF, "Partition size: total: %zu, used: %zu", total, used);
+    }
+
+    // Register event handler for starting http server
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
         &_connect_handler, &_server));
 }
@@ -12,35 +43,67 @@ HttpServer::HttpServer(void)
 void HttpServer::_connect_handler(void* arg, esp_event_base_t event_base,
     int32_t event_id, void* event_data)
 {
-    httpd_handle_t* server = (httpd_handle_t*)arg;
-    if (*server == NULL) {
+    httpd_handle_t* _server = static_cast<httpd_handle_t*>(arg);
+    if (*_server == NULL) {
         ESP_LOGI(TAG, "Starting webserver");
-        *server = HttpServer::start_webserver();
+        *_server = HttpServer::start_webserver();
     }
 }
 
 /* An HTTP GET handler */
-esp_err_t root_get_handler(httpd_req_t* req)
+static esp_err_t root_get_handler(httpd_req_t* req)
 {
-    /* Send response with custom headers and body set as the
-     * string passed in user context*/
-    const char* resp_str = (const char*)req->user_ctx;
-    httpd_resp_send(req, resp_str, strlen(resp_str));
+    char html_file[] = "/spiffs/favicon.ico";
 
-    // /* After sending the HTTP response the old HTTP request
-    //  * headers are lost. Check if HTTP request headers can be read now. */
-    // if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
-    //     ESP_LOGI(TAG, "Request headers lost");
-    // }
+    // Check if destination file exists before renaming
+    ESP_LOGI(HttpServer::TAG_SPIFF, "Trying to open %s", html_file);
+    FILE* fd = fopen(html_file, "r");
+    if (!fd) {
+        ESP_LOGE(HttpServer::TAG_SPIFF, "Failed to open %s", html_file);
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+    ESP_LOGI("SPIFFS", "File %s opened successfully", html_file);
+
+    char buffer[512];
+    size_t bytes_read;
+    struct stat file_stat;
+
+    if (stat(html_file, &file_stat) != 0) {
+        ESP_LOGE(HttpServer::TAG, "Failed to get file stats");
+        fclose(fd);
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+
+    ESP_LOGI(HttpServer::TAG, "Sending file: %s (%ld bytes)...", html_file,
+        file_stat.st_size);
+
+    // Set the content type header
+    httpd_resp_set_type(req, "image/x-icon");
+
+    // Send the file
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), fd)) > 0) {
+        if (httpd_resp_send_chunk(req, buffer, bytes_read) != ESP_OK) {
+            ESP_LOGE(HttpServer::TAG, "File sending failed!");
+            fclose(fd);
+            httpd_resp_send_500(req);
+            return ESP_FAIL;
+        }
+    }
+    fclose(fd);
+
+    // Always finish sending the response
+    httpd_resp_send_chunk(req, NULL, 0);
+
+    ESP_LOGI(HttpServer::TAG, "File sending complete");
     return ESP_OK;
 }
 
-httpd_uri_t root_dir = { .uri = "/",
+httpd_uri_t root_dir = { .uri = "/favicon.ico",
     .method = HTTP_GET,
     .handler = root_get_handler,
-    /* Let's pass response string in user
-     * context to demonstrate it's usage */
-    .user_ctx = const_cast<char*>(web_page.c_str()) };
+    .user_ctx = NULL };
 
 httpd_handle_t HttpServer::start_webserver(void)
 {
@@ -50,6 +113,7 @@ httpd_handle_t HttpServer::start_webserver(void)
     // Start the httpd server
     ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
     if (httpd_start(&server, &config) == ESP_OK) {
+
         // Set URI handlers
         ESP_LOGI(TAG, "Registering URI handlers");
         httpd_register_uri_handler(server, &root_dir);
--- 10_NVS_http/main/http.h
+++ 10_NVS_http/main/http.h
@@ -1,25 +1,20 @@
 #pragma once
 
-#include "http_web_page.h"
-#include <cstddef>
-#include <sys/param.h>
-
-#include "esp_err.h"
-#include "esp_event.h"
-#include "esp_log.h"
-#include "esp_netif.h"
-#include "esp_system.h"
-#include "freertos/FreeRTOS.h"
+#include "esp_err.h" // IWYU pragma: keep
+#include "esp_event.h" // IWYU pragma: keep
+#include "esp_http_server.h"
+#include "esp_log.h" // IWYU pragma: keep
+#include "esp_spiffs.h" // IWYU pragma: keep
 #include "freertos/task.h"
-#include "nvs.h"
-#include "nvs_flash.h"
-
+#include <cstddef> // IWYU pragma: keep
 #include <esp_http_server.h>
+#include <sys/param.h>
+#include <sys/stat.h>
 
 namespace Http_NS {
 class HttpServer {
 private:
-    httpd_handle_t _server = nullptr;
+    static httpd_handle_t* _server;
     static void _connect_handler(void* arg, esp_event_base_t event_base,
         int32_t event_id, void* event_data);
     static void _disconnect_handler(void* arg, esp_event_base_t event_base,
@@ -31,5 +26,6 @@ public:
     HttpServer(void);
     ~HttpServer(void) = default;
     constexpr static const char* TAG = "HTTPServer";
+    constexpr static const char* TAG_SPIFF = "SPIFFS";
 };
 } // Namespace Http_NS
